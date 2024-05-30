b7c88fe05d28dd9697b1cda27d5421c6973a6602
Ozan Akkaşoğlu <ozan470@hotmail.com>
2024-05-30 13:14:41 +0300

    Proje dosyası ekle.

---
+++ Worker-Service-Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Worker_Service_Sample;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<WorkerService>();
+    })
+    .Build();
+
+await host.RunAsync();
---
+++ Worker-Service-Sample/WorkerService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Worker_Service_Sample
+{
+    public class WorkerService : BackgroundService
+    {
+        private readonly ILogger<WorkerService> _logger;
+
+        public WorkerService(ILogger<WorkerService> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken); // 1 saniye bekler
+            }
+        }
+    }
+}
